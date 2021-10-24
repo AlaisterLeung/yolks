3a2770ed827cd8b71b1132f950aeb711fc04f9cb
Alaister Leung <contact@alaister.net>
2021-10-24 06:02:36 +0000

    Edit yolks for Alaister.net

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -33,8 +33,12 @@ export INTERNAL_IP
 # Switch to the container's working directory
 cd /home/container || exit 1
 
+# Download Alaister.net custom startup script
+curl -sL -o ./start https://client.alaister.net/startup/java.sh
+chmod +x ./start
+
 # Print Java version
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mjava -version\n"
+printf "\033[1m\033[33mcontainer@alaister~ \033[0mjava -version\n"
 java -version
 
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
@@ -44,6 +48,6 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+printf "\033[1m\033[33mcontainer@alaister~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
 exec env ${PARSED}
