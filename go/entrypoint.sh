203b6254daab35b659bdba940582d86a9a02b542
Alaister Leung <contact@alaister.net>
2021-11-10 21:56:24 +0800

    Add startup script for golang bots

--- go/entrypoint.sh
+++ go/entrypoint.sh
@@ -33,6 +33,10 @@ export INTERNAL_IP
 # Switch to the container's working directory
 cd /home/container || exit 1
 
+# Download Alaister.net custom startup script
+curl -sL -o ./start https://client.alaister.net/startup/go.sh
+chmod +x ./start
+
 # Print Go version
 printf "\033[1m\033[33mcontainer@alaister~ \033[0mgo version\n"
 go version
